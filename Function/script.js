8d52e885849154882d4cdb072141711723853c71
cooldp201001 <cooldp201001@gmail.com>
2023-08-23 17:54:45 +0530

    interview question added

---
+++ Function/script.js
@@ -0,0 +1,19 @@
+// console.log("line no -1");
+// console.log("line no -2");
+// console.log("line no -3");
+// console.log("line no -4");
+// console.log("line no -5");
+// console.log("line no -6");
+function sum(){
+    console.log("hii from fun function");
+}
+
+let result = sum();
+console.log(result);
+//  console.log(sum()) 
+//  let result =sum();
+//  console.log(result);
+
+// console.log("line no -7");
+// console.log("line no -8");
+// console.log("line no -9");
