e8b7c3510f41b6a10972e2cb95ac802dbfc3bc8c
cooldp201001 <cooldp201001@gmail.com>
2023-08-24 17:48:40 +0530

    return topic completed

--- Function/script.js
+++ Function/script.js
@@ -1,19 +1,3 @@
-// console.log("line no -1");
-// console.log("line no -2");
-// console.log("line no -3");
-// console.log("line no -4");
-// console.log("line no -5");
-// console.log("line no -6");
-function sum(){
-    console.log("hii from fun function");
+if(11%2){
+    alert("executed");
 }
-
-let result = sum();
-console.log(result);
-//  console.log(sum()) 
-//  let result =sum();
-//  console.log(result);
-
-// console.log("line no -7");
-// console.log("line no -8");
-// console.log("line no -9");
