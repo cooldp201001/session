b07b20fc37fe07e6bc0ee0a926f2cb20087ea547
cooldp201001 <cooldp201001@gmail.com>
2023-08-29 17:29:14 +0530

    MCQs file added

--- Function/script.js
+++ Function/script.js
@@ -1,3 +1,57 @@
-if(11%2){
-    alert("executed");
+//function expression
+// let funExpression = function (a,b) {
+//    console.log("hello from function expression output is given below");
+//     document.write(a+b);
+// }
+
+
+// funExpression(4,6);
+
+
+//arrow function
+/* 1 version
+let arrowFunction = (p1,p2) => {
+    console.log("hello from arrow function output is given below");
+    document.write(p1+p2);
+}
+
+arrowFunction(10,20)
+*/
+
+/* Example 2: Arrow Function with One Argument
+let arrowFunction2 = p1 => {
+    console.log("hello from arrow function output is given below");
+    document.write(p1*p1);
+}
+
+arrowFunction2(150);
+*/
+
+//Example 3: Arrow Function with No Argument
+
+let arrowFunction2 = () => {
+    // console.log("hello from arrow function output is given below");
+    document.write("hello from arrow function output is given below");
+}
+
+
+
+//  let arrowFunction3=()=> {
+    
+//  };
+
+// let returnValue= arrowFunction3();
+// console.log(returnValue);
+
+
+const myFunc=() => {}
+
+{
+
+function fun(){
+ let a;
+}
+
+
+
 }
