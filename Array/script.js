09dad4885ef7a74244e77bb7d64d9005bcead694
cooldp201001 <cooldp201001@gmail.com>
2023-09-07 18:28:09 +0530

    array introduction

---
+++ Array/script.js
@@ -0,0 +1,21 @@
+//array
+let  ArrVar = [2342,2323,23423,235,23,235,25,235,235,2352]
+console.log(ArrVar);
+
+// let simpleVar = 2342,2323,23423,235,23,235,25,235,235,2352,2352;
+
+/* 
+ 1st = 5;
+ let student1="xyx";
+ let student2="xyx";
+ let student3="xyx";
+ let student4="xyx";
+ let student5="xyx";
+
+ 2nd = 20;
+ 20 varible create and store the names of the students 20 times
+ console.log > 20 times
+
+8th = 1000;
+*/
+
