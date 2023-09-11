f206b5f9554bf809fa3054c550621da5b805b3a6
cooldp201001 <cooldp201001@gmail.com>
2023-09-11 17:57:50 +0530

    basic concept clear

--- Array/script.js
+++ Array/script.js
@@ -1,6 +1,6 @@
 //array
 let  ArrVar = [2342,2323,23423,235,23,235,25,235,235,2352]
-console.log(ArrVar);
+// console.log(ArrVar);
 
 // let simpleVar = 2342,2323,23423,235,23,235,25,235,235,2352,2352;
 
@@ -19,3 +19,31 @@ console.log(ArrVar);
 8th = 1000;
 */
 
+let Arr = [1234,213512,1235,1235,2345];
+
+let elm=Arr[3]
+// console.log(Arr);
+
+// key,value ,property, method(function)
+
+let obj ={
+ key:"value",   
+ fun: function(){
+    console.log("hii");
+ }
+}
+
+// let demoVar= [2342,234]
+//  console.log( typeof  demoVar);
+
+// mix type values
+ let demoAray = [1,234,22,235]
+//  console.log(demoAray.length-1);
+ console.log(demoAray);
+
+ demoAray.push(1);
+ console.log(demoAray);
+ demoAray.push(2342)
+ console.log(demoAray);
+ demoAray.pop();
+ console.log(demoAray);
