89d6c58cee1c64b24913b6a6ef7170e1a19a1d4c
cooldp201001 <cooldp201001@gmail.com>
2023-09-17 18:37:10 +0530

    arrays practice question added

---
+++ Array/iteration/script.js
@@ -0,0 +1,19 @@
+// foreach()
+
+let num=[1,2,3,4,354,2345,"adgadd",2342,23,54,245,34,4,245,];
+
+num.forEach(greeting)
+
+function greeting(elm){
+    // console.log(elm);
+  console.count()
+
+}
+
+
+// let num2= [1,2,3,4,5]
+
+// let result = num2.splice(2,0,"string","string2");
+
+// console.log(num2);
+// console.log(result);
